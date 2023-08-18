4c988e029bc82136761d21264e102bffa68de6d1
kokoro37 <141924039+kokoro37@users.noreply.github.com>
2023-08-19 00:29:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let nav =document.querySelector("#navArea");
+let btn =document.querySelector(".toggle-button");
+let mask =document.querySelector("#mask");
+
+btn.onclick = () =>{
+    nav.classList.toggle("open")
+}
+
+mask.onclick= () =>{
+    nav.classList.toggle("open")
+}
+
